4a8c296ed9beb7b133b9b29dd1b29bf1f846d83b
LandisBing <38116850+LandisBing@users.noreply.github.com>
2018-04-13 01:28:31 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include "storage.h"
 using namespace std;
 
 
 int main(){
+    
+    vector<Transaction> sampleSort;
+    string inputFile;
+    cout << "Please enter the name of the Bank Account File: " << endl;
+    cin >> inputFile;
+    BankAccount mainAccount(inputFile);
+    mainAccount.parseFileData();
+    sampleSort = mainAccount.getAllTransactions();
 
-cout << "Hello World!" << endl;
+    mainAccount.addNewTransaction();
 
-
+    mainAccount.closeFile();
+  
 return 0;
 }
