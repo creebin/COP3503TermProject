2240628dd550cd1041f543799eecae3fddc8e721
misaacs21 <32146041+misaacs21@users.noreply.github.com>
2018-04-15 23:05:46 -0400

    Update ui.cpp

--- ui.cpp
+++ ui.cpp
@@ -2,6 +2,27 @@
 #include <iostream>
 using namespace std;
 
+//blueprint class, forgot what it's called: graphs. two types: bar and pie (if we wanna die, but it'd look so pretty)
+    //function: ratio the data in terms of each other
+    //function: "crop"/simplify the data in such a way that it'll fit the screen
+    //function: print the data like a graph
+
+//class: interface
+    //function: set welcome ascii upon entering program
+    //function: set closing ascii upon leaving program
+    //function: user menu
+    //function: can you delete previous stuff from the command line??? function to do that so it acts more like a window
+
+//class: data formatting
+    //function: category sort with nice, vertical category formatting
+    //function: budget remainder "graph"
+    //function: bill reminder, like:
+      /*      ______________      ____________
+          !! |   BILL  DUE  | !! |   DATE     | !!
+      /*
+    //functions to pleasingly display the rest of the data manip methods, can't think of a fun way for the others
+    
+    
 int main() {
   std::cout << "Hello world!" << std::endl;
   
