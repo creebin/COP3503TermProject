68ab9f1cc2bc8fc13e3769b238330bf28809bf35
LandisBing <38116850+LandisBing@users.noreply.github.com>
2018-04-18 00:37:45 -0400

    Setter functions added and additional spacing
    The setter functions here can be easily removed but I decided to add them since we may want to change Date to a class with private member variables in the future. It also gives us more lines. If you decide to remove them be sure to change the Transaction::setDate function as well.

--- Date.h
+++ Date.h
@@ -2,17 +2,28 @@
 #define GROUPPROJECT_DATE_H
 #include <string>
 
-using namespace std;
+struct Date {
 
-struct Date
-{
-    Date(int month, int day, int year);
-    Date(Date const & newDate);
-    Date();
     int month;
     int day;
     int year;
+
+    Date(int month, int day, int year);
+
+    Date(Date const &newDate);
+
+    Date();
+
     string toString();
-    int compareTo(Date other)const;
+
+    int compareTo(Date other) const;
+
+    void setDay(int newDay);
+
+    void setMonth(int newMonth);
+
+    void setYear(int newYear);
 };
+
+
 #endif //GROUPPROJECT_DATE_H
