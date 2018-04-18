14b9a66c755a301a7a315eab2a2298ecf1f86791
LandisBing <38116850+LandisBing@users.noreply.github.com>
2018-04-18 00:49:02 -0400

    Storage.h changed to Budget.h
    -  BankAccount class renamed to Budget
    -  Date struct incorporated into Budget
    -  Budget class changed from header only to cpp and header files
    Note: Transaction has to be listed before Budget because transaction objects need to be defined before Budget can use them.

---
+++ Budget.h
@@ -0,0 +1,56 @@
+
+#ifndef BUDGET_BUDGET_H
+#define BUDGET_BUDGET_H
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "Date.h"
+
+using namespace std;
+//Transaction class to group the data together
+class Transaction {
+private:
+    //Member variables
+    string name;
+    string category;
+    double amount;
+    Date transactionDate;
+
+public:
+    //Member functions
+    string getCategory();
+
+    void setCategory(string newCategory);
+
+    double getAmount();
+
+    void setAmount(double newAmount);
+
+    Date getDate();
+
+    void setDate(int month, int day, int year);
+
+    Transaction();
+};
+
+//Bank account class to group the transactions together
+class Budget {
+private:
+    ifstream fileData;
+    string fileName;
+    vector<Transaction> allTransactions;
+public:
+
+    void parseFileData();
+
+    vector<Transaction> getAllTransactions();
+
+    void addNewTransaction();
+
+    void closeFile();
+
+    explicit Budget(string inputName);
+};
+
+#endif //BUDGET_BUDGET_H
