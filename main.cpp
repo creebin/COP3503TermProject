3ee6b070dc7a52a807a15cc302614581eb107c5d
nciar1 <32080906+nciar1@users.noreply.github.com>
2018-04-19 16:50:15 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 
 using namespace std;
 
+// Checks if user input is a valid option. This is achieved by using a for loop to analyze the user input string. If any value
+// is not a string it will return input as false.
 bool inputChecker(string userInput) {
 	bool isNum;
 	for (int i = 0; i < userInput.length(); i++) {
@@ -21,6 +23,7 @@ bool inputChecker(string userInput) {
 }
 
 
+//Prints out user menu and asks takes user input for the menu options.
 void userMenu() {
 	bool validInput = false;
 	while (!validInput) {
@@ -43,11 +46,16 @@ void userMenu() {
 		cout << "|8. Exit                         |\n";
 		cout << "+--------------------------------+\n";
 		cin >> userMenuInput;
+		
+		// Sends user input for the menu option chosen to the inputChecker function to ensure the input is a number.
+		// If the input is not valid it will print an error message and reprint the menu.
 		bool validInput = inputChecker(userMenuInput);
 		if (!validInput) {
 			cout << "Invalid input please try again\n";
 		}
 		else {
+		// If the input is a number it will parse the input from a string to a integer and use this number to direct
+		// the user to the chosen menu function.
 			int numInput = stoi(userMenuInput);
 			switch (numInput) {
 				case 1:
@@ -92,6 +100,7 @@ void userMenu() {
 		}
 	}
 
+// Prints welcome menu that will ask user if they want to create a new budget, change existing budget, or use an existing budget.
 void welcomeMenu() {
 	bool validInput = false;
 	while (!validInput) {
