ae75f6e1c93209a7b41a7628afb61a56b8a264dc
nciar1 <32080906+nciar1@users.noreply.github.com>
2018-04-19 16:39:11 -0400

    Update ui.cpp

--- ui.cpp
+++ ui.cpp
@@ -120,42 +120,7 @@ void BarGraph::printGraph(vector<int> money, vector<string> category, vector<int
 
 }
 
-void UserInterface::welcomeMenu() {
-	bool validInput = false;
-	while (!validInput) {
-		string welcomeInput;
-		cout << "1. Create New Budget\n";
-		cout << "2. Change Existing Budget\n";
-		cout << "3. Use Existing Budget\n";
-		cout << "4. Exit Program\n";
-		cin >> welcomeInput;
-		validInput = inputChecker(welcomeInput);
-		if (!validInput) {
-			cout << "Invalid input please try again\n";
-		}
-		else {
-			int numInput = stoi(welcomeInput);
-			switch (numInput) {
-			case 1:
-				break;
 
-			case 2:
-				break;
-
-			case 3:
-				break;
-
-			case 4:
-				break;
-
-			default:
-				cout << "Invalid input please try again";
-				validInput = false;
-			}
-		}
-
-	}
-}
 
 void UserInterface::welcomeScreen() {
 	cout << " ██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗\n";
@@ -176,60 +141,6 @@ void UserInterface::closingScreen() {
 	cout << "╚═════╝  ╚═════╝  ╚═════╝ ╚═════╝ ╚═════╝    ╚═╝   ╚══════╝╚═╝\n";
 }
 
-void UserInterface::userMenu() {
-	bool validInput = false;
-	while (!validInput) {
-		string userMenuInput;
-		cout << "1. Print\n";
-		cout << "2. Change budget amount\n";
-		cout << "3. Add expense or transcation\n";
-		cout << "4. Delete expense or transaction\n";
-		cout << "5. Sort\n";
-		cout << "6. Save\n";
-		cout << "7. Exit\n";
-		cin >> userMenuInput;
-		bool validInput = inputChecker(userMenuInput);
-		if (!validInput) {
-			cout << "Invalid input please try again\n";
-		}
-		else {
-			int numInput = stoi(userMenuInput);
-			switch (numInput) {
-				case 1:
-
-					break;
-
-				case 2:
-					break;
-
-				case 3:
-
-					break;
-
-				case 4:
-
-					break;
-
-				case 5:
-
-					break;
-
-				case 6:
-					break;
-
-				case 7: 
-					closingScreen();
-					return;
-					
-				default: 
-					 cout << "Invalid input please try again";
-					 validInput = false;
-
-				}
-			}
-
-		}
-	}
 
 
 void UserInterface::clearScreen() {
