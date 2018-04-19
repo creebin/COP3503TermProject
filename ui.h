245a30543efca915e2ea167bb55c20ee198e3b3a
misaacs21 <32146041+misaacs21@users.noreply.github.com>
2018-04-19 13:04:34 -0400

    Update ui.h

--- ui.h
+++ ui.h
@@ -1,44 +1,41 @@
-//header
-#include <iostream>
-#include<vector>
+#ifndef UI_H
+#define UI_H
 
+#include <vector>
+#include <string>
+using namespace std;
 
-
-
-class Graphs
-{
+class BarGraph {
 private:
-    vector<string> categoryNames;
-    vector<int> moneyAmount;
-    int totalBudget;
-    const int fitScreen = 2;
-
+	int fitScreen;
 public:
-    vector<int> ratioData(vector<int>,int);//what inputs, and should it output a vector or smth? should it be completely fleshed out up here since won't change?
-    vector<int> cropData(vector<int>);//should be completely fleshed out up here?
-    void printGraph(vector<int>, vector<string>);
-};
-
-
-class PieGraph : public Graphs
-{
-public:
-    void printGraph()
-    {
-
-    }
+	BarGraph();
+	vector<int> ratioData(vector<int>, int);
+	vector<int> cropData(vector<int>);
+	void printGraph(vector<int>, vector<string>, vector<int>);
 };
 
 class UserInterface
 {
 public:
-    void welcomeMenu(); //do they want to create a new budget or use an existing budget?
-    void closingScreen();
-    void userMenu();
-    //if choose sort->sorting menu in sorting
-    //take in input
-    void clearScreen();
-    void inputChecker(string);
+	void welcomeMenu(); //do they want to create a new budget or use an existing budget?
+	void welcomeScreen();
+	void closingScreen();
+	void userMenu();
+	//if choose sort->sorting menu in sorting
+	//take in input
+	void clearScreen();
 
 };
 
+class DataFormatting
+{
+private:
+public:
+	//what do they want printed -> function. Categories printed nearly? What's remaining in the budget as a 'battery' graph?
+	void billReminder();
+};
+
+bool inputChecker(string);
+
+#endif
